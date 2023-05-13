24c16acffedbcc641ac0e97832cdbc8e51c01acf
Facundo García Gallo <alu0101406329@ull.edu.es>
2023-05-13 09:09:33 +0000

    Avances en la página y subpáginas

---
+++ src/js/tools.js
@@ -0,0 +1,8 @@
+
+// Esta función desliza la página hacia arriba para aportar una mejor experiencia de usuario además de una mejor gestión de la accesibilidad.
+function deslizarArriba() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
